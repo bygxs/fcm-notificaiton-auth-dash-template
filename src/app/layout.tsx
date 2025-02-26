097f6f60bca2d6cecfd86bf5f810defd5d484560
bygxs <biniyamyohannes@icloud.com>
2025-02-26 20:38:36 +0100

    pwa completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+
+import {ServiceWorkerRegistration} from  "@/app/components/ServiceWorkerRegistration";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,12 +26,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+ 
+  
+
   return (
     <html lang="en">
+      <head><link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>{children}</ThemeProvider>
+        <ServiceWorkerRegistration />
         <footer className="py-2 fixed bottom-0 left-0 right-0 flex justify-center items-center py-4 bg-gray-800 text-white">
           Version: {version}
         </footer>
